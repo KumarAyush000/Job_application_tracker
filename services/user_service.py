97943cadf506ebc54ce1a66b216eca2bca282ea4
KumarAyush000 <krayush123000@gmail.com>
2026-01-15 22:44:32 +0530

    updated the code to generate unique user ID's

--- services/user_service.py
+++ services/user_service.py
@@ -1,4 +1,15 @@
 
 
 def generate_user_id(users):
-    pass
+    """
+    Generates a new unique user ID in the format U001, U002, ...
+    """
+    if not users:
+        return "U001"
+
+    # Extract numeric part of IDs and find max
+    last_id = max(users.keys())
+    last_number = int(last_id[1:])
+    new_number = last_number + 1
+
+    return f"U{new_number:03d}"
